cfc9b1fd9f84ec14d7ba8d61ea1bbd4ef279c598
liuyibao <liuyibao@styd.cn>
2018-02-05 13:45:24 +0800

    add bash directory

---
+++ bash/c.php
@@ -0,0 +1,2 @@
+<?php
+echo "this is c.php", PHP_EOL;
--- bash/xargs.sh
+++ bash/xargs.sh
@@ -2,5 +2,5 @@
 cat xargs-data.txt | xargs
 cat xargs-data.txt | xargs -n3
 cat xargs-data.txt | xargs -n1
-echo "nameXnameXnameXname" | xargs -dX
-cd /Users/liuyibao/Documents/DockerVolumes/mysql-5.6.35/ && ls | grep "\-slow\.log" | xargs -I {} tail -f {}
+#cd /Users/liuyibao/Documents/DockerVolumes/mysql-5.6.35/ && ls | grep "\-slow\.log" | xargs -I {} tail -f {}
+ls | grep "\.php" | xargs -n1 -I {} php {}
